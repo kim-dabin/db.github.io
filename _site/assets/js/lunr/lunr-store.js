89162084102bcc21744e748c59f7f5457c959c0c
kim-dabin <pippisgoing@gmail.com>
2020-01-08 05:30:35 +0900

    update Gemfile

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "minimal mistakes 테마를 이용해 github.io 블로그 구축하기",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/hello/",
+        "teaser":null}]
